ed700f2eefa67e5f6484d6f63a213827c0ec545b
onyxgeek <onyxpeek@gmail.com>
2023-05-23 15:58:40 +0300

    feat: add the 3d object

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import Navbar from './components/Header/Navbar';
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+
+function Model(props) {
+  const { scene } = useGLTF("/pc.glb");
+  return <primitive object={scene} {...props} />
+}
 
 function App() {
   return (
-    <div className="wrapper">
-      <Navbar/>
-      <div className="wrapper__content">
-         <h1 className='noSelect'>Hello, I'm an developer based in Romania!</h1>
-      </div>
-    </div>
+    <>
+     <Navbar/>
+      <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} style={{"position": "absolute"}}>
+      <color attach="background" args={["#101010"]} />
+      <PresentationControls speed={3.5} global zoom={.5} polar={[-0.1, Math.PI / 4]}>
+        <Stage environment={"sunset"}>
+          <Model scale={0.01} />
+        </Stage>
+      </PresentationControls>
+    </Canvas>
+    </>
+
   );
 }
 
