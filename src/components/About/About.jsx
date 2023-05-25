b5dc4d3384d007ae910e10737ff97b34bfd5810a
onyxgeek <onyxpeek@gmail.com>
2023-05-25 16:38:31 +0300

    feat: add the base style

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './About.css';
-import PersonalPhoto from '../../assets/img/profile-img.jpg'
+import PersonalPhoto from '../../assets/img/profile-img.jpg';
+
 const About = () => {
   return (
     <section className='aboutpage'>
       <div className="wrapper__content">
-        <h1 className='present__header'>Hello, I'm a developer based in Romania!</h1>
+            <div className='present__header'>
+                <h1 className='present__text'>
+                    Hello, I'm a developer based in Romania!
+                </h1>
+        </div>
         <div className="general-info">
-          <h2>General Info</h2>
-          <p>Some general information about me</p>
-          <img src={PersonalPhoto} alt="" />
+          <div className='general-presentation'>
+            <h2 className='name-header'>Gorobet Nicolae</h2>
+            <p className='descrition-personal'>Digital Craftsman(Student, Developer)</p>
+          </div>
+          <img src={PersonalPhoto} alt="profile-img" className='profile__image' />
         </div>
         <div className="personal-info">
           <h2>Personal Info</h2>
@@ -22,3 +29,4 @@ const About = () => {
 };
 
 export default About;
+
