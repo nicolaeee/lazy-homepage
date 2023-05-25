a47b1982e1aca18ac333b208683cfc0b6eaf85b3
onyxgeek <onyxpeek@gmail.com>
2023-05-25 15:45:25 +0300

    feat: add the profile image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import './About.css'
-
+import React from 'react';
+import './About.css';
+import PersonalPhoto from '../../assets/img/profile-img.jpg'
 const About = () => {
   return (
-    <div className="wrapper__content">
-    <h1 className='noSelect'>Hello, I'm a developer based in Romania!</h1>
-  </div>
-  )
-}
+    <section className='aboutpage'>
+      <div className="wrapper__content">
+        <h1 className='present__header'>Hello, I'm a developer based in Romania!</h1>
+        <div className="general-info">
+          <h2>General Info</h2>
+          <p>Some general information about me</p>
+          <img src={PersonalPhoto} alt="" />
+        </div>
+        <div className="personal-info">
+          <h2>Personal Info</h2>
+          <p>Some personal information about me</p>
+          <button>Click Me</button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
