50a2d3fe971da6a4859ec5b145a0c2c50d972d81
onyxgeek <onyxpeek@gmail.com>
2023-05-26 00:51:39 +0300

    feat: finish the works page

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -1,61 +1,71 @@
-import React from 'react'
-import "./MyWork.css"
-import { FaGithub } from 'react-icons/fa'
-import ShelterPhoto from '../../assets/img/shelter.png'
-import PlantsPhoto from '../../assets/img/plants.png'
-import UniversityPort from '../../assets/img/UP.png'
-import CsAciee from '../../assets/img/cs-aciee.png'
+import React from 'react';
+import "./MyWork.css";
+import { FaGithub } from 'react-icons/fa';
+import ShelterPhoto from '../../assets/img/shelter.png';
+import PlantsPhoto from '../../assets/img/plants.png';
+import UniversityPort from '../../assets/img/UP.png';
+import CsAciee from '../../assets/img/cs-aciee.png';
 
 const MyWork = () => {
   return (
     <section className='workpage'>
-  <div className="wrapper__content">
-    <h1>Works</h1>
-    <div className="wrapper__blocks">
-      <div className="block-content">
-        <a href="work1-link">
-          <img src={ShelterPhoto} alt="Shelter" className='portfolio-image'/>
-          <p>Shelter - A house for all pets </p>
-          <p>
-            Work 1 Details
-            <FaGithub />
-          </p>
-        </a>
+      <div className="wrapper__content">
+        <h1>Works</h1>
+        <div className="wrapper__blocks">
+          <div className="block-content">
+            <a href="https://rolling-scopes-school.github.io/onyxgeek-JSFE2023Q1/shelter/main/index.html#">
+              <img src={ShelterPhoto} alt="Shelter" className='portfolio-image' />
+            </a>
+            <a href="https://rolling-scopes-school.github.io/onyxgeek-JSFE2023Q1/shelter/main/index.html#">
+              <p>Shelter - A house for all pets </p>
+            </a>
+            <a href="work1-link">
+              <p>
+                Source  &nbsp;
+                <FaGithub />
+              </p>
+            </a>
+          </div>
+          <div className="block-content">
+            <a href="https://portfolio-university.vercel.app/">
+              <img src={UniversityPort} alt="University" className='portfolio-image' />
+            </a>
+            <a href="https://portfolio-university.vercel.app/">
+              <p>University Portfolio-my notes from the University</p>
+            </a>
+            <a href="https://github.com/nicolaeee/Portfolio-University">
+            Source  &nbsp;
+                <FaGithub />
+            </a>
+          </div>
+          <div className="block-content">
+            <a href="https://cs-aciee.vercel.app/">
+              <img src={CsAciee} alt="Cs-Aciee" className='portfolio-image' />
+            </a>
+            <a href="https://cs-aciee.vercel.app/">
+              <p>CS-ACIEE - Clubul Studentiolor FACIEE</p>
+            </a>
+            <a href="https://github.com/nicolaeee/cs-aciee">
+            Source  &nbsp;
+                <FaGithub />
+            </a>
+          </div>
+          <div className="block-content">
+            <a href="https://rolling-scopes-school.github.io/onyxgeek-JSFEPRESCHOOL2022Q4/plants/">
+              <img src={PlantsPhoto} alt="Plants" className='portfolio-image' />
+            </a>
+            <a href="https://rolling-scopes-school.github.io/onyxgeek-JSFEPRESCHOOL2022Q4/plants/">
+              <p>Plants - A landing page about one product</p>
+            </a>
+            <a href="https://github.com/nicolaeee/plants/tree/gh-pages">
+            Source  &nbsp;
+                <FaGithub />
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="block-content">
-        <a href="work2-link">
-        <img src={UniversityPort} alt="University" className='portfolio-image'/>
-          <p>Work 2 Description</p>
-          <p>
-            Work 2 Details
-            <FaGithub />
-          </p>
-        </a>
-      </div>
-      <div className="block-content">
-        <a href="work3-link">
-        <img src={CsAciee} alt="Cs-Aciee" className='portfolio-image'/>
-          <p>Work 3 Description</p>
-          <p>
-            Work 3 Details
-            <FaGithub />
-          </p>
-        </a>
-      </div>
-      <div className="block-content">
-        <a href="work4-link">
-        <img src={PlantsPhoto} alt="Plants" className='portfolio-image'/>
-          <p>Work 4 Description</p>
-          <p>
-            Work 4 Details
-            <FaGithub />
-          </p>
-        </a>
-      </div>
-    </div>
-  </div>
-</section>
-  )
-}
+    </section>
+  );
+};
 
-export default MyWork
+export default MyWork;
