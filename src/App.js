9d467c6f816566e0a3bab5904a793c47f64b2123
onyxgeek <onyxpeek@gmail.com>
2023-05-25 23:33:42 +0300

    feat: add the work page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Header/Navbar';
 import CanvasComponent from './components/CanvasComponent';
 import About from './components/About/About';
+import MyWork from './components/MyWork/MyWork';
 
 function App() {
   return (
     <div className="wrapper">
-        <Navbar />
-        <CanvasComponent />
-        <About/>
+      <Navbar />
+      <CanvasComponent />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/my-work" element={<MyWork />} />
+      </Routes>
     </div>
   );
 }
