b378d093bfd27d15890cac0392d942c31b05a949
onyxgeek <onyxpeek@gmail.com>
2023-05-26 01:47:55 +0300

    feat: last commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import './About.css';
-import './AboutMobile.css'
+import './AboutMobile.css';
 import PersonalPhoto from '../../assets/img/profile-img.jpg';
 import { Link } from 'react-router-dom';
 import { FaGithub, FaFacebook, FaLinkedin, FaHeart, FaCodewars } from 'react-icons/fa';
 import MyWork from '../MyWork/MyWork';
-
+import MyCV from '../../assets/GOROBET NICOLAE-JUNIOR-DEV-CV.pdf';
 
 const About = () => {
+  const handleCVClick = () => {
+    window.open(MyCV);
+  };
+
   return (
     <section className='aboutpage'>
       <div className="wrapper__content">
-      <div className='present__header'>
-                <h1 className='present__text'>
-                    Hello, I'm a developer based in Romania!
-                </h1>
+        <div className='present__header'>
+          <h1 className='present__text'>
+            Hello, I'm a developer based in Romania!
+          </h1>
         </div>
         <div className="general-info">
           <div className='general-presentation'>
@@ -26,7 +30,7 @@ const About = () => {
         <div className="personal-info">
           <h2 className='job-header'>Work</h2>
           <p className='description-profesional'>I am a Computer Science student with a deep passion for coding and software development. As someone who loves to explore the intricacies of programming, I am constantly seeking opportunities to expand my knowledge and skills in this field. Whether it's developing innovative solutions or diving into complex algorithms, I find great joy in the art of crafting software. With a strong dedication to continuous learning and a drive to solve real-world problems through technology, I am excited to contribute to the ever-evolving world of software development."</p>
-          <button className='button-cv'>My CV</button>
+          <button className='button-cv' onClick={handleCVClick}>My CV</button>
         </div>
       </div>
       <div className="bio-info">
@@ -69,4 +73,3 @@ const About = () => {
 };
 
 export default About;
-
