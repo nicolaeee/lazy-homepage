bc5059bb2614f7ed0bbc6ebd40e2d3ea75bc0861
onyxgeek <onyxpeek@gmail.com>
2023-05-25 09:06:42 +0300

    fix: fix the navbar

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import './App.css';
 import Navbar from './components/Header/Navbar';
 import CanvasComponent from './components/CanvasComponent';
 
-
-
 function App() {
   return (
     <div className="wrapper">
-      <Navbar/>
-      <CanvasComponent/>
-      <div className="wrapper__content">
-         <h1 className='noSelect'>Hello, I'm an developer based in Romania!</h1>
+      <div className="navbar-wrapper">
+        <Navbar />
       </div>
-
+      <div className="canvas-wrapper">
+        <CanvasComponent />
+      </div>
+      <div className="wrapper__content">
+        <h1 className='noSelect'>Hello, I'm a developer based in Romania!</h1>
+      </div>
     </div>
   );
 }
 
 export default App;
+
